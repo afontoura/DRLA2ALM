27e9de57bd944bb3679924d77da027639f5afebc
afontoura <fontoura.alr@gmail.com>
2019-08-30 23:17:40 -0300

    Base package
    Base package

--- gym-ALM/gym_ALM/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_ALM.envs.ALM_env import ALMEnv
--- gym-ALM/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-
-setup(name = 'gym_ALM',
-      version = '0.1',
-      install_requires = ['gym', 'numpy', 'pandas'])
